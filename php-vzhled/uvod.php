1ef91074beb8fd0e81195872714c82db347085b9
Fido <david.fido.fiedler@gmail.com>
2013-11-26 22:35:44 +0100

    old

--- php-vzhled/uvod.php
+++
@@ -1,9 +0,0 @@
-
-		<div id="uvod">
-			<p id="nadpis_uvodu">Vítejte na stránkách Vločky...</p>
-			<p id="text_uvodu">
-				Jsme oddíl světlušek, vlčat, skautů, skautek a R&amp;R, dohromady je nás okolo 
-				100 VLOČEK - jsme taková malá lavina a klubovnu máme u Seneckého rybníka (Plzeň - Bolevec). Oslavili 
-				jsme čtrnácté narozeniny...
-			</p>
-		</div>
